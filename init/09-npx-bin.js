2ec5a769948472a85118e77428e0a8b1a4805ea4
Brendan Graetz <bguiz@users.noreply.github.com>
2024-08-01 12:39:00 +0800

    feat: copy util/util.js
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

--- init/09-npx-bin.js
+++ init/09-npx-bin.js
@@ -1,15 +1,35 @@
 #!/usr/bin/env node
 
 const process = require('node:process');
+const path = require('node:path');
+const fs = require('node:fs/promises');
+
+const processCwd = process.cwd();
+const processArgv = process.argv;
 
 async function hederaTutorialDemoBaseTemplateRun() {
-    console.log('hederaTutorialDemoBaseTemplateRun', process.argv);
-    const processCwd = process.cwd();
+    console.log('hederaTutorialDemoBaseTemplateRun');
     console.log({
         __dirname,
         __filename,
         processCwd,
+        processArgv,
     });
+    const subCmd = process.argv[2];
+    switch (subCmd) {
+        case 'update-util':
+            await updateUtil();
+            break;
+        default:
+            console.error('Unrecognised sub-command:', subCmd);
+            break;
+    };
+}
+
+async function updateUtil() {
+    const fromFilePath = path.resolve(__dirname, '../util/util.js');
+    const toFilePath = path.resolve(processCwd, './util/util.js');
+    await fs.copyFile(fromFilePath, toFilePath);
 }
 
 hederaTutorialDemoBaseTemplateRun();
