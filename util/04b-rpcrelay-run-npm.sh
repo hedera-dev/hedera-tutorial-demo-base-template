ca524f7c681a9fe2ca6c9c46ad0da3adcc8121e7
Brendan Graetz <bguiz@users.noreply.github.com>
2024-08-19 13:30:00 +0800

    feat: add script to run RPC relay via NPM instead of docker
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

--- util/04b-rpcrelay-run-npm.sh
+++ util/04b-rpcrelay-run-npm.sh
@@ -8,16 +8,25 @@ export RPC_RELAY_VERSION=$( curl -L \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/hashgraph/hedera-json-rpc-relay/releases?per_page=1" |
   jq -r ".[].tag_name" |
-  cut -c2-
+  cut -c1-
 )
 echo "🟣 RPC Relay version: ${RPC_RELAY_VERSION}"
-export RPC_RELAY_DOCKER_IMAGE="ghcr.io/hashgraph/hedera-json-rpc-relay:${RPC_RELAY_VERSION}"
 
-# run the RPC relay via its docker image
-docker run \
-  --rm \
-  --publish 7546:7546 \
-  --publish 8546:8546 \
-  --publish 8547:8547 \
-  --env-file ${DIR}/../.rpcrelay.env \
-  "${RPC_RELAY_DOCKER_IMAGE}"
+cd ..
+
+git clone \
+  --depth 1 \
+  --branch "${RPC_RELAY_VERSION}" \
+  "https://github.com/hashgraph/hedera-json-rpc-relay.git" \
+  "./rpcrelay"
+
+cd ./rpcrelay
+
+# install dependencies, including sub-projects
+echo "🟣 Set up hedera-json-rpc-relay..."
+npm install
+npm run build
+
+# run the relay
+echo "🟣 Run hedera-json-rpc-relay..."
+npm run start
